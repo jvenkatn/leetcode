eafcb4a6c19b2d694db907035d43a6647352fcca
josyulavn <venkat.nj@samsung.com>
2020-03-28 20:45:51 +0530

    initial setup files

---
+++ venkatleetcode/venkatleetcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  venkatleetcode
+//
+//  Created by Venkat on 28/03/20.
+//  Copyright © 2020 venkat. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
